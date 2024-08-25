a3e6172bc1e0ab5e090dddbfb76c0dfc494b5f76
Mohammad Tanvir <mohammad_tanvir@epam.com>
2024-08-25 21:50:36 +0530

    created P01 to learn terraform

---
+++ P01-Setup-EC2-Instance/Setup-EC2-Instance.md
@@ -0,0 +1,3 @@
+# Create a EC2 Instance using terraform
+
+# Create a Security group using terraform
---
+++ P01-Setup-EC2-Instance/main.tf
@@ -0,0 +1,45 @@
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_instance" "demo-ec2" {
+  ami = "ami-066784287e358dad1"
+  instance_type = "t3.medium"
+
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+
+  tags = {
+    Name = "demo-ec2-instance"
+  }
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "terraform-firewall"
+  description = "Managed from Terraform"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv6" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "${aws_eip.eip.public_ip}/32"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
+resource "aws_eip" "eip" {
+  instance = aws_instance.demo-ec2.id
+  domain   = "vpc"
+}
+
+output "ec2_public_ip" {
+  value = aws_eip.eip.public_ip
+}
+
+output "App_URL" {
+  value = "http://${aws_eip.eip.private_ip}:8080"
+}
